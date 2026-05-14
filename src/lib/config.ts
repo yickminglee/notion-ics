d6d62aad96daa843b9ec4245f681bbf1483a2d08
Ming Lee <52941636+yickminglee@users.noreply.github.com>
2026-05-14 16:54:27 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,7 +2,7 @@ import { ICalEventBusyStatus } from 'ical-generator';
 import type { QueryDataSourceParameters } from '@notionhq/client/build/src/api-endpoints';
 
 export default {
-  filter: undefined,
+    filter: undefined,
 	dateProperty: 'Date',
 	titleProperty: 'Event name',
 	busy: ICalEventBusyStatus.BUSY
